8f617938df990e86397dedc8b8f24bccfb00b225
Laurens Nolting <contact@laurensnol.com>
2024-02-08 00:45:03 +0100

    nvim: fix issue where nvim-treesitter.configs was not found

--- dot_config/nvim/lua/plugins/language.lua
+++ dot_config/nvim/lua/plugins/language.lua
@@ -4,6 +4,10 @@ return {
     "nvim-treesitter/nvim-treesitter",
     version = false,
     build = ":TSUpdate",
+    init = function(plugin)
+      require("lazy.core.loader").add_to_rtp(plugin)
+      require("nvim-treesitter.query_predicates")
+    end,
     config = function()
       require("nvim-treesitter.configs").setup {
         ensure_installed = {
