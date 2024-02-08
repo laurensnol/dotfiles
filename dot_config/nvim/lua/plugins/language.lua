b1f83887118e26b23923c3403f77c31458744eac
Laurens Nolting <contact@laurensnol.com>
2024-02-08 01:31:22 +0100

    Revert "nvim: fix issue where nvim-treesitter.configs was not found"
    This reverts commit 8f617938df990e86397dedc8b8f24bccfb00b225.

--- dot_config/nvim/lua/plugins/language.lua
+++ dot_config/nvim/lua/plugins/language.lua
@@ -4,10 +4,6 @@ return {
     "nvim-treesitter/nvim-treesitter",
     version = false,
     build = ":TSUpdate",
-    init = function(plugin)
-      require("lazy.core.loader").add_to_rtp(plugin)
-      require("nvim-treesitter.query_predicates")
-    end,
     config = function()
       require("nvim-treesitter.configs").setup {
         ensure_installed = {
